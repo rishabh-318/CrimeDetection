5e7c032cec864957ee9e52bad64ded898b09bbcd
__Rishabh__318 <122962130+rishabh-318@users.noreply.github.com>
2026-05-27 02:35:30 +0530

    Update footer copyright name to Rishabh Rana

--- Website/frontend/src/components/footer/Footer.jsx
+++ Website/frontend/src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
             <Link href={"#"}><Image className='w-[50px] h-[50px]' width={1000} height={1000} src={"/x.png"}></Image></Link>
             <Link href={"#"}><Image className='w-[40px] h-[40px]' width={1000} height={1000} src={"/discord.png"}></Image></Link>
         </div>
-        <h3 className='text-center font-light text-white pt-5'>© 2024 Abhay Semwal All rights reserved.</h3>
+        <h3 className='text-center font-light text-white pt-5'>© 2024 Rishabh Rana All rights reserved.</h3>
     </footer>
   )
 }
